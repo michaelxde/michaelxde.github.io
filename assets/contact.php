a50b577b495053e3ccfa57cef264868a368993b5
michaelxde <64078412+michaelxde@users.noreply.github.com>
2021-09-13 02:42:00 -0400

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -51,7 +51,7 @@ if(get_magic_quotes_gpc()) {
 
 
 //$address = "HERE your email address";
-$address = "info@removals.com";
+$address = "michaeltende1@gmail.com";
 
 
 // Below the subject of the email
